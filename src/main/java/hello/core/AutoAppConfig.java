ebb512d07b76f0a99b9388eb1fcb5b9aec9061ef
coconutFlipFlop <fgvcxg00@gmail.com>
2023-02-22 22:16:40 +0900

    2023-02-22 6. 컴포넌트 스캔 - 필터, 중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepositiry;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,5 +14,11 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters =  @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    //2023-02-22 6. 컴포넌트 스캔 - 필터, 중복 등록과 충돌
+    @Bean(name = "memoryMemberRepository")
+    MemberRepositiry memberRepositiry() {
+        return new MemoryMemberRepository();
+    }
 
 }
